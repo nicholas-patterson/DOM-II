378f63fe3d278dd13990accd1103951663385f89
Nicholas Patterson <nicholaspatterson@Nicholass-MBP.home>
2019-06-18 16:31:21 -0400

    Finished main project working on stretch

--- js/index.js
+++ js/index.js
@@ -1,1 +1,62 @@
 // Your code goes here
+
+const mainHeading = document.querySelector(".nav-container .logo-heading");
+
+mainHeading.addEventListener("mouseenter", event => {
+  mainHeading.style.color = "crimson";
+});
+
+mainHeading.addEventListener("mouseleave", event => {
+  mainHeading.style.color = "black";
+});
+
+const nav = document.querySelectorAll("header .nav a");
+nav.forEach(link => {
+  link.addEventListener("click", event => {
+    mainHeading.textContent = link.textContent;
+    event.preventDefault();
+  });
+
+  link.addEventListener("mouseenter", event => {
+    link.style.color = "crimson";
+    event.stopPropagation();
+  });
+
+  link.addEventListener("mouseleave", event => {
+    link.style.color = "black";
+  });
+});
+
+const navContainer = document.querySelector(".main-navigation");
+
+navContainer.addEventListener("mouseleave", event => {
+  mainHeading.textContent = "Fun Bus";
+});
+
+// dbl click
+const footer = document.querySelector(".footer p");
+footer.addEventListener("dblclick", event => {
+  footer.innerHTML = "<p>Nicholas Patterson <span>&copy;</span> 2019</p>";
+});
+
+const welcomeText = document.querySelector(".intro h2");
+
+welcomeText.addEventListener("drag", event => {
+  mainHeading.textContent = "Put Me Down";
+});
+
+const body = document.querySelector("body");
+const letsGo = document.querySelector(".content-section .text-content .letsGo");
+
+body.addEventListener("mouseover", event => {
+  if (event.screenX > 3912 && event.screenX < 4716) {
+    event.stopPropagation();
+    letsGo.innerHTML = "<h1>Main Content</h1>";
+  } else {
+    letsGo.innerHTML = "<h1>GET OFF MY CONTAINER</h1>";
+  }
+});
+
+window.addEventListener("scroll", event => {
+  nav.style.backgroundColor = "crimson";
+});
