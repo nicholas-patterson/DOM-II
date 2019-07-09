3d289fabe18d520692d46183acce321020a5b4a2
Nicholas Patterson <nicholaspatterson@Nicholass-MBP.home>
2019-07-09 16:03:06 -0400

    event handlers used working on stretch task

--- js/index.js
+++ js/index.js
@@ -1,1 +1,52 @@
 // Your code goes here
+
+const mainHeading = document.querySelector(".logo-heading");
+
+mainHeading.addEventListener("mouseover", event => {
+  mainHeading.style.fontSize = "70px";
+});
+
+mainHeading.addEventListener("mouseout", event => {
+  mainHeading.style.fontSize = "40px";
+});
+
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", event => {
+    link.style.color = "crimson";
+    event.preventDefault();
+  });
+});
+
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(button => {
+  button.addEventListener("click", event => {
+    button.textContent = "You Clicked On Me !";
+    button.style.backgroundColor = "crimson";
+  });
+});
+
+const body = document.querySelector("body");
+
+window.addEventListener("scroll", event => {
+  body.style.backgroundColor = "aliceblue";
+});
+
+const funBus = document.querySelector("#photo");
+
+funBus.addEventListener("dblclick", event => {
+  funBus.style.display = "none";
+});
+
+secondaryHeading = document.querySelectorAll("h2");
+
+secondaryHeading.forEach(heading => {
+  heading.addEventListener("drag", event => {
+    mainHeading.textContent = "PUT ME DOWN !";
+  });
+  heading.addEventListener("mouseover", event => {
+    mainHeading.textContent = "Fun bus";
+  });
+});
